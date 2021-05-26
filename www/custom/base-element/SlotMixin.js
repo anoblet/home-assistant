962be44683a4bc02f4e47eb98b6f4a417c51d718
Andrew Noblet <andrewbnoblet@gmail.com>
2021-05-26 11:16:20 -0400

    feat(HA): wip

--- www/custom/base-element/SlotMixin.js
+++ www/custom/base-element/SlotMixin.js
@@ -23,12 +23,10 @@ export const SlotMixin = (superClass) =>
 
       [...this.shadowRoot.querySelectorAll("slot")].map((slot) => {
         slots[slot.getAttribute("name")] = slot;
+
+        slot.addEventListener("slotchanged", this.requestUpdate);
       });
 
       this._slots = slots;
-    }
-
-    _onSlotChange() {
-      this.requestUpdate();
     }
   };
